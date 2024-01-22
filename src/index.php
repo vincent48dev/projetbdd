02eed616e6e3eab23e838263b146b15210ae661f
vincent48dev <147699347+vincent48dev@users.noreply.github.com>
2024-01-22 16:52:11 +0100

    maj

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
   <script src="https://kit.fontawesome.com/862e2d197c.js" crossorigin="anonymous"></script>
 </head>
 <body class = "bg-gray-400">
-  <h1 class="text-3xl font-bold underline mt-5 text-center mb-5">
+  <h1 class="text-3xl font-bold underline mt-5 text-center mb-5 decoration-blue-800 text-blue-800">
     PROJET BDD
   </h1>
   <?php include("pdo.php");?>
