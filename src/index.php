5ad269e7caf4ed8c662be131a3147c033716b491
vincent48dev <147699347+vincent48dev@users.noreply.github.com>
2024-01-23 16:49:17 +0100

    maj

--- src/index.php
+++ src/index.php
@@ -12,11 +12,67 @@
     PROJET BDD
   </h1>
   <?php include("pdo.php");?>
-<?php
-$result = $pdo->query("SELECT * FROM favori");
-$favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
-?>
+<!-- zone de filtrage -->
+<?php   
+          // Affichage (SELECT) :
+      
+       
 
+
+      if(isset($_GET['categorie'],$_GET['categorie']) && $_GET['categorie'] !== "none" && $_GET['domaine'] !== "none"){
+        $result = $pdo->query("SELECT * FROM `favori` INNER JOIN `cat_fav` ON favori.id_fav=cat_fav.id_fav INNER JOIN `domaine` ON favori.id_dom=domaine.id_dom INNER JOIN `categorie` ON cat_fav.id_cat=categorie.id_cat WHERE categorie.id_cat=".$_GET['categorie']." AND domaine.id_dom=".$_GET['domaine'].";");
+        $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+      }else{
+        if(isset($_GET['domaine']) && $_GET['domaine'] !== "none" && $_GET['categorie'] == "none"){
+        $result = $pdo->query("SELECT * FROM `favori` INNER JOIN `domaine` ON favori.id_dom=domaine.id_dom WHERE domaine.id_dom=".$_GET['domaine']." ORDER BY `id_fav` ASC;");
+        $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+      }else{
+        if(isset($_GET['categorie']) && $_GET['categorie'] !== "none" && $_GET['domaine'] == "none"){
+          $result = $pdo->query("SELECT * FROM `favori` INNER JOIN `cat_fav` ON favori.id_fav=cat_fav.id_fav INNER JOIN `domaine` ON favori.id_dom=domaine.id_dom INNER JOIN `categorie` ON cat_fav.id_cat=categorie.id_cat WHERE categorie.id_cat=".$_GET['categorie'].";");
+          $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+        }else{
+          $result = $pdo->query("SELECT * FROM `favori` INNER JOIN `domaine` ON favori.id_dom=domaine.id_dom ORDER BY `id_fav` ASC;");
+          $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+        }
+
+      }
+        
+      }
+    ?> 
+    <!-- zone de filtrage -->
+﻿
+
+ <section class = "w-full flex justify-center text-black">
+    <form class = "py-8" action=""method="GET">
+      <select class="ml-4" name="categorie" id="formulaire">
+        <?php
+          $data = $pdo->query("SELECT * FROM `categorie` ORDER BY `id_cat` ASC;");
+          $categories = $data->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <option value="none">categorie</option>
+        <?php
+        foreach($categories as $categorie){?>
+        <option class="" value="<?php echo $categorie['id_cat'] ?>"><?php echo $categorie['nom_cat'] ?></option>
+        <?php
+        };
+        ?>
+      </select>
+      <select class="ml-4" name="domaine" id="formulaire">
+        <?php
+          $data = $pdo->query("SELECT * FROM `domaine` ORDER BY `id_dom` ASC;");
+          $domaines = $data->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <option value="none">domaine</option>
+        <?php
+        foreach($domaines as $domaine){?>
+        <option class="" value="<?php echo $domaine['id_dom'] ?>"><?php echo $domaine['nom_dom'] ?></option>
+        <?php
+        };
+        ?>
+      </select>
+      <button class="bg-blue-400 ml-5 border-2 px-4">filtrer</button>
+    </form>
+  </section>
   <section class="w-full flex justify-center mt-28">
     <table class="w-5/6">
         <tr class = "border-2 bg-blue-400">
